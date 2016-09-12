edccda5fa2bec05f300e578340eac4b26b6b3cd7
mjuradom <mjurado@student.unimelb.edu.au>
2016-09-12 14:39:55 +1000

    Primer mensaje

---
+++ Assets/MyTextInput.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class MyTextInput : NetworkBehaviour
+{
+
+    InputField input;
+    InputField.SubmitEvent se;
+    public Text output;
+
+    void Start()
+    {
+        input = gameObject.GetComponent<InputField>();
+        se = new InputField.SubmitEvent();
+        se.AddListener(SubmitInput);
+        input.onEndEdit = se;
+
+    }
+
+    void Update()
+    {
+        //chat(input.text);
+
+    }
+    
+
+    private void SubmitInput(string arg0)
+    {
+        string currentText = output.text; //maybe add ToString()?
+        string newText = currentText + "\n" + arg0;
+        output.text = newText;
+        input.text = "";
+        input.ActivateInputField();
+    }
+    /*
+    public void chat(string message)
+    {
+        CmdChat(message);
+    }
+
+    [Command]
+    void CmdChat(string message)
+    {
+        RpcChat(message);
+    }
+
+    [ClientRpc]
+    void RpcChat(string message)
+    {
+        output.text += message;
+        output.text += "\n";
+    }*/
+
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Text chatText;
+    private InputField chatInput;
+    private Button button;
+
+    void start()
+    {
+        chatText = GameObject.Find("Text").GetComponent<Text>();
+        chatInput = GameObject.Find("InputField").GetComponent<InputField>();
+        button = GameObject.Find("Button").GetComponent<Button>();
+    }
+
+    void Update()
+    {
+        if (chatInput == null || chatText == null || button == null)
+        {
+            Debug.Log("chatInput or chatText or button are null");
+            chatInput = GameObject.Find("InputField").GetComponent<InputField>();
+            chatText = GameObject.Find("Text").GetComponent<Text>();
+            button = GameObject.Find("Button").GetComponent<Button>();
+
+
+        }
+        else
+        {
+            Debug.Log("chatInput is set");
+            Debug.Log(">>" + chatInput.text + "<<");
+            InputField.OnChangeEvent change = chatInput.onValueChanged;
+            //button.onClick;
+            chatText.text = chatInput.text;
+        }
+    }
+}
